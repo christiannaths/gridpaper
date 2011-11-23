81ad74cb224b0c01e464f939040e7529b6e6f8e3
Christian Naths <christiannaths@gmail.com>
2011-11-23 12:32:09 -0700

    modified template copy method

--- lib/gridpaper.rb
+++ lib/gridpaper.rb
@@ -9,13 +9,37 @@ module Gridpaper
 
     # Add Gridpaper to your project
     def initialize(path)
+      templates_path = File.join(File.dirname(__FILE__), '../', 'templates', 'sass', 'gridpaper')
+      destination_path = File.join(path, 'stylesheets', 'sass', 'gridpaper')
+
+      template_files = [
+        '_all.sass',
+        '_forms.sass',
+        '_grid.sass',
+        '_reset.sass'
+      ]
+
+
       # Create the main stylesheets directory
-      destination_path = File.join(path, "stylesheets")
-      FileUtils::Verbose.mkdir(destination_path)
+      if exists?(destination_path)
+        puts "skipped: #{ destination_path } (exists)"
+      else
+        FileUtils.mkdir_p(destination_path)
+        puts "created: #{ destination_path }"
+      end
 
       # Copy templates to destination directory
-      templates_path = File.join(File.dirname(__FILE__), '../', 'templates', 'sass')
-      FileUtils::Verbose.cp_r(templates_path, destination_path)
+      template_files.each do |file|
+        source_file = File.join(templates_path, file)
+        new_file = File.join(destination_path, file)
+        if exists?(new_file)
+          puts "skipped: #{ new_file } (exists)"
+        else
+          FileUtils.cp_r(new_file, destination_path)
+          puts "created: #{ new_file }"
+        end
+      end
+
     end
 
   end
